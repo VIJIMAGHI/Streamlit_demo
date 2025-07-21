bdada7c6cd3687bb1b93d9dae831a816253d3a55
VIJIMAGHI <144479274+VIJIMAGHI@users.noreply.github.com>
2025-07-21 14:16:42 +0400

    Add files via upload

---
+++ stcalculator.py
@@ -0,0 +1,15 @@
+import streamlit as st
+num1= st.number_input("enter num1")
+num2=st.number_input("enter num2")
+operations=st.selectionbox("operation select:",["add","sub","mul","div"])
+if st.button("calculate"):
+	if operations=="add":
+		result=num1+num2
+	elif operations=="sub":
+		result=num1-num2
+	elif opertaions=="mul":
+		result=num1*num2
+	elif operations=="div"
+		result=num1/num2 if num2!=0 else "cannot divide"
+st.success(f"result is {result}")
+	
